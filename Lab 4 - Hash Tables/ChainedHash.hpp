e09ea53f687bb3a3b31f1c54f5b35d85ab5d6100
RoscoeTheDog <54086262+RoscoeTheDog@users.noreply.github.com>
2021-04-20 15:02:30 -0700

    init

---
+++ Lab 4 - Hash Tables/ChainedHash.hpp
@@ -0,0 +1,10 @@
+//
+// Created by admin on 4/20/2021.
+//
+
+#ifndef LAB_4___HASH_TABLES_CHAINEDHASH_HPP
+#define LAB_4___HASH_TABLES_CHAINEDHASH_HPP
+
+#include "HashTable.hpp"
+
+#endif //LAB_4___HASH_TABLES_CHAINEDHASH_HPP
