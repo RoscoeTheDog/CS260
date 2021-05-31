e46aa025412d4e97e089579e5293aedbbea4d5fc
RoscoeTheDog <54086262+RoscoeTheDog@users.noreply.github.com>
2021-05-30 22:01:51 -0700

    Updated lab so it uses 'implicit' linked lists instead of std::list. add, remove, find and everything else refactored to follow this.

--- Lab 4 - Hash Tables/ChainedHash.cpp
+++ Lab 4 - Hash Tables/ChainedHash.cpp
@@ -5,115 +5,210 @@
 #include "ChainedHash.hpp"
 
 void ChainedHash::addItem(std::string &value) {
-    if (length >= (size - 1) / 2)
-        resize(size * 2);
 
-    key = encode(value);
-    ind = hash(key);
+	if (length >= (size - 1) / 2) {
+		resize(size * 2);
+	}
 
-    // pre increment and then push link.
-    if (table[ind].empty())
-        ++length;
+	key = encode(value);
+	index = hash(key);
 
-    table[ind].push_front(value);
+	// get the node at the table
+	Node *n = table[index];
+
+	// create new node, passing in value.
+	Node *_new = new Node(value);
+
+	// check if a link exists at the table
+	if (n) {
+		// if it does, traverse to tail
+		while (n->next) {
+			n = n->next;
+		}
+
+		// link together old tail to new tail.
+		n->next = _new;
+		_new->prev = n;
+	}
+		// if table doesn't have a valid node, just copy new one into table.
+	else {
+		table[index] = _new;
+	}
+
+	// update table length.
+	length++;
 }
 
 
 void ChainedHash::removeItem(const std::string &value) {
 
-    key = encode(value);
+	key = encode(value);
 
-    ind = hash(key);
+	index = hash(key);
 
-    if (!table[ind].empty())
-        for (std::list<std::string>::iterator it = table[ind].begin(); it != table[ind].end(); ++it) {
-            if (*it == value) {
-                table[ind].erase(it);
-                break;  // needed to escape from endless loop if deleted == last node.
-            }
+	Node *n = table[index];
+	Node *previous = nullptr;
+	Node *next = nullptr;
 
-        }
-    else {
-        table[ind].push_front(value);
-        --length;
-    }
+	while (n) {
 
+		if (n->value == value) {
+
+			if (n->prev) {
+				previous = n->prev;
+			}
+
+			if (n->next) {
+				next = n->next;
+			}
+
+			delete n;
+
+			if (previous) {
+				previous->next = next;
+			}
+			if (next) {
+				next->prev = previous;
+			}
+
+			break;
+		}
+
+		n = n->next;
+	}
+
+	// reset old index to null.
+	table[index] = nullptr;
+
+	// update table length
+	length--;
 }
 
 bool ChainedHash::findItem(const std::string &value) {
 
-    key = encode(value);
+	key = encode(value);
 
-    ind = hash(key);
+	index = hash(key);
 
-    if (!table[ind].empty())
-        for (std::list<std::string>::iterator it = table[ind].begin(); it != table[ind].end(); ++it) {
-            if (*it == value)
-                return true;
-        }
+	Node *n = table[index];
 
-    return false;
+	while (n) {
+
+		if (n->value == value) {
+			return true;
+		}
+
+		// continue until value is found or tail is hit
+		if (n->next) {
+			n = n->next;
+		}
+
+	}
+
+	return false;
 }
 
 std::string ChainedHash::displayTable() {
-    std::stringstream S;
 
-    for (unsigned i = 0; i < size; ++i) {
-        if (!table[i].empty()) {
-            for (std::list<std::string>::iterator it = table[i].begin(); it != table[i].end(); ++it) {
-                S << *it << " ";
-            }
-        }
-    }
+	std::stringstream S;
 
-    return S.str();
+	Node *n = nullptr;
+	for (int i = 0; i < size; ++i) {
+
+		n = table[i];
+
+		// iterate through links and push to string buffer
+		while (n) {
+			S << n->value << " ";
+			n = n->next;
+		}
+
+	}
+	return S.str();
 }
 
 void ChainedHash::resize(unsigned size) {
 
-    if (size < this->size)
-        throw std::out_of_range("new size cannot be made smaller or data will be lost");
+	if (size < this->size) {
+		throw std::out_of_range("new size cannot be made smaller or data will be lost");
+	}
 
-    std::list<std::string> *table_ = new std::list<std::string>[size];
+	// create new table
+	Node **extended;
+	extended = new Node*[size];
+	// initialize all Node * to null
+	for (int i = 0; i < size; ++i) {
+		extended[i] = nullptr;
+	}
 
-    // begin rehashing/deep copying
-    for (unsigned i = 0; i < size - 1; ++i) {
-        // validate if in old table range or not.
-        if (i < this->size) {
-            // is empty??
-            if (not table[i].empty()) {
-                // iter through all nodes
-                for (std::list<std::string>::iterator it = table[i].begin(); it != table[i].end(); ++it) {
-                    // rehash values
-                    key = encode(*it);
-                    ind = hash(key);
-                    // I've been doing push front, pop back (FIFO). Here I'll push back to maintain order since Iter starts at front.
-                    table_[ind].push_back(*it);
-                }
-            }
-        }
-    }
+	// save old size since rehashing requires updating over it.
+	int oldSize = this->size;
+	// update size before rehashing.
+	this->size = size;
 
-    delete[] table;
-    table = table_;
-    this->size = size;
+	// iterate through the array
+	for (int i = 0; i < oldSize; ++i) {
+
+		// check if table has a valid pointer.
+		if (table[i]) {
+
+			// get ptr from table
+			Node *copyNode = table[i];
+
+			// rehash each link from head of pointer
+			while (copyNode) {
+
+				key = encode(copyNode->value);
+				index = hash(key);
+
+				// see if any entry exists on the new table index or not.
+				if (extended[index]) {
+
+					// get the head from the new index
+					Node *n = extended[index];
+
+					// find tail
+					while (n) {
+						n = n->next;
+					}
+
+					// copy at the tail
+					n = copyNode;
+				}
+					// if no node exists @ new index beforehand, simply copy to table.
+				else {
+					extended[index] = copyNode;
+				}
+
+				copyNode = copyNode->next;
+			}
+
+		}
+
+	}
+
+	delete[]table;
+	table = extended;
+	this->size = size;
+
 }
 
 int &ChainedHash::hash(int s) {
-    // keeping all of this local is fine, just remember these are not the class member's key.
-    ind = key % size;
+	// keeping all of this local is fine, just remember these are not the class member's key.
+	index = key % size;
 
-    return ind;
+	return index;
 }
 
 long ChainedHash::encode(std::string s) {
-    long weight = 0;
-    char c;
 
-    for (char i : s) {
-        c = i;
-        weight += static_cast<int>(c);
-    }
+	long weight = 0;
+	char c;
 
-    return weight;
+	for (char i : s) {
+		c = i;
+		weight += static_cast<int>(c);
+	}
+
+	return weight;
 }
--- Lab 4 - Hash Tables/ChainedHash.hpp
+++ Lab 4 - Hash Tables/ChainedHash.hpp
@@ -16,28 +16,73 @@ class ChainedHash {
 private:
     unsigned size;
     unsigned length;
-    std::list<std::string> *table;
+
+	typedef struct Node {
+
+		Node(std::string value) {
+			this->value = value;
+		}
+
+		Node * prev = nullptr;
+		Node * next = nullptr;
+		std::string value;
+	} Node;
+
+    Node **table = nullptr;
 
     // having key be a class member is not necessary but makes code a bit easier to understand.
     int key; // key == the 'weight' of the string in ASCII
-    int ind; // ind == the actual hashed table index of the key.
+    int index; // index == the actual hashed table index of the key.
 
 public:
 
     ChainedHash() {
         size = 11;
         length = 0;
-        table = new std::list<std::string>[size];
+	    table = new Node*[size];
+
+	    for (int i = 0; i < size; ++i) {
+	    	table[i] = nullptr;
+	    }
     };
 
     ChainedHash(unsigned size) {
         this->size = size;
         length = 0;
-        table = new std::list<std::string>[this->size];
+	    table = new Node*[size];
+
+	    for (int i = 0; i < size; ++i) {
+		    table[i] = nullptr;
+	    }
     }
 
+    //TODO: resolve endless next links
     ~ChainedHash() {
-        delete[] table;
+
+    	for (int i = 0; i < size; ++i) {
+
+    		Node *head = table[i];
+
+    		// while head
+    		while (head) {
+
+    			// copy pointer so we can iterate through easier
+    			Node *n = head;
+
+    			// go to last node
+    			while (n->next) {
+				    n = n->next;
+    			}
+    			// clear link from prev
+    			if (n->prev) {
+    				n->prev = nullptr;
+    			}
+    			// delete tail
+    			delete n;
+    		}
+
+    	}
+        delete[]table;
     }
 
     void addItem(std::string &value);
